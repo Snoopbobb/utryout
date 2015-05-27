d9fc079f468d263cdbaec74ccb72780925bc804c
Robert Tabor <bobtabor@q.com>
2015-05-27 14:43:50 -0700

    made profile template

--- resources/views/tryouts/index.blade.php
+++ resources/views/tryouts/index.blade.php
@@ -2,13 +2,29 @@
 
 @section('content')
 	<div class="container">
-			<h1>Tryouts</h1>
+	
+	@if( count($tryouts) > 0 )
+		@if(!empty($sport))
+			<h1>{{ ucwords($sport) }} Tryouts</h1>
+		@else
+			<h1>All Tryouts</h1>
+		@endif
 
-			@foreach ($tryouts as $tryout)
-				<div class="tryout">
-					<span class="sport">{{ $tryout->sport }}</span> <h3>{{ $tryout->organization }} | {{ $tryout->age }} and under | {{ date('D F d, Y', strtotime($tryout->date)) }}, {{ date('g:i A', strtotime($tryout->time)) }} | {{ $tryout->city }}, {{ $tryout->state }}</h3>
-					<a href="/tryouts/{{ $tryout->id }}">Additional Information</a>
-				</div>
-			@endforeach
+		@foreach ($tryouts as $tryout)
+			<div class="tryout">
+				<span class="sport"><a href="/tryouts/{{ $tryout->sport }}">{{ $tryout->sport }}</a></span> <h3>{{ $tryout->organization }} | Ages: {{ $tryout->age1 }} - {{ $tryout->age2 }} | {{ date('D F d, Y', strtotime($tryout->date)) }}, {{ date('g:i A', strtotime($tryout->time)) }} | {{ $tryout->city }}, {{ $tryout->state }}</h3>
+				<a href="/tryouts/{{ $tryout->id }}">Additional Information</a>
+			</div>
+		@endforeach
+	@else
+		<h1>We're Sorry There Are No Upcoming {{ ucwords($sport) }} Tryouts 
+
+			@if(!empty($state))
+				in {{ strtoupper($state) }}
+			@endif 
+
+			Posted, Please Check Back Soon!</h1>
+	@endif
+
 	</div>
 @endsection
--- resources/views/tryouts/profile.blade.php
+++ resources/views/tryouts/profile.blade.php
@@ -3,13 +3,16 @@
 @section('content')
 	<div class="container">
 		<h1>Your Posted Tryouts</h1>
-
-		@foreach ($tryouts as $tryout)
-			<div class="tryout">
-				<span class="sport"><a href="/tryouts/{{ $tryout->sport }}">{{ $tryout->sport }}</a></span> <h3>{{ $tryout->organization }} | Ages: {{ $tryout->age1 }} - {{ $tryout->age2 }} | {{ date('D F d, Y', strtotime($tryout->date)) }}, {{ date('g:i A', strtotime($tryout->time)) }} | {{ $tryout->city }}, {{ $tryout->state }}</h3>
-				<a href="/tryouts/{{ $tryout->id }}">Additional Information</a>
-			</div>
-		@endforeach
+		@if ( count($tryouts) > 0 )
+			@foreach ($tryouts as $tryout)
+				<div class="tryout">
+					<span class="sport"><a href="/tryouts/{{ $tryout->sport }}">{{ $tryout->sport }}</a></span> <h3>{{ $tryout->organization }} | Ages: {{ $tryout->age1 }} - {{ $tryout->age2 }} | {{ date('D F d, Y', strtotime($tryout->date)) }}, {{ date('g:i A', strtotime($tryout->time)) }} | {{ $tryout->city }}, {{ $tryout->state }}</h3>
+					<a href="/tryouts/{{ $tryout->id }}">Additional Information</a>
+				</div>
+			@endforeach
+		@else
+			<a href="tryouts/create" class="btn btn-primary">Post A Tryout</a>
+		@endif
 
 	</div>
 @endsection

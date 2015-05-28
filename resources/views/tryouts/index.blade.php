ecc03138c18db05a5ff9689751068bfb3a6f5918
Robert Tabor <bobtabor@q.com>
2015-05-27 22:34:59 -0700

    fixed links

--- resources/views/tryouts/index.blade.php
+++ resources/views/tryouts/index.blade.php
@@ -12,8 +12,8 @@
 
 		@foreach ($tryouts as $tryout)
 			<div class="tryout">
-				<span class="sport"><a href="/tryouts/{{ $tryout->sport }}">{{ $tryout->sport }}</a></span> <h3>{{ $tryout->organization }} | Ages: {{ $tryout->age1 }} - {{ $tryout->age2 }} | {{ date('D F d, Y', strtotime($tryout->date)) }}, {{ date('g:i A', strtotime($tryout->time)) }} | {{ $tryout->city }}, {{ $tryout->state }}</h3>
-				<a href="/tryouts/{{ $tryout->id }}">Additional Information</a>
+				<span class="sport"><a href="{{ url('/tryouts/$tryout->sport') }}">{{ $tryout->sport }}</a></span> <h3>{{ $tryout->organization }} | Ages: {{ $tryout->age1 }} - {{ $tryout->age2 }} | {{ date('D F d, Y', strtotime($tryout->date)) }}, {{ date('g:i A', strtotime($tryout->time)) }} | {{ $tryout->city }}, {{ $tryout->state }}</h3>
+				<a href="{{ url('/tryouts/$tryout->id') }}">Additional Information</a>
 			</div>
 		@endforeach
 	@else

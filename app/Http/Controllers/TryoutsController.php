21ca9d36812b0fe7e4d2adaba4b596f5c361c4b7
Robert Tabor <bobtabor@q.com>
2015-05-28 17:42:53 -0700

    test:

--- app/Http/Controllers/TryoutsController.php
+++ app/Http/Controllers/TryoutsController.php
@@ -34,7 +34,7 @@ class TryoutsController extends Controller {
 	public function showId($sport, $state, $city, $id){
 		$id = intval($id);
 
-		$tryouts = Tryout::all()->where('id', intval($id));
+		$tryouts = Tryout::all()->where('sport', $sport);
 
 		if (count($tryouts) < 1) {
 			return Redirect::back()->with('message','Sorry, that post was not found. Please check back later.');

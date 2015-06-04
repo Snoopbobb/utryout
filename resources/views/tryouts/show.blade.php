5f3a5939fc8e27b7caa859a5ec40a5fa333540e3
Robert Tabor <bobtabor@q.com>
2015-06-04 00:48:09 -0700

    fixed title class css

--- resources/views/tryouts/show.blade.php
+++ resources/views/tryouts/show.blade.php
@@ -10,21 +10,21 @@
 				<h1>{{ ucwords($tryout->organization) }}</h1>
 			</div>
 			<h1><a href="http://{{ ucwords($tryout->website) }}">Visit {{ $tryout->organization }} Website</a></h1>
-			<h1><span class="title">Sport:</span> {{ ucwords($tryout->sport) }}</h1>
-			<h1><span class="title">Age:</span> {{ $tryout->age1 . 'U' }}</h1>
+			<h1><span class="tryout-title">Sport:</span> {{ ucwords($tryout->sport) }}</h1>
+			<h1><span class="tryout-title">Age:</span> {{ $tryout->age1 . 'U' }}</h1>
 			<h1>{{ $tryout->age2 }}</h1>
 			<h1>{{ $tryout->age3 }}</h1>
 			<h1>{{ $tryout->age4 }}</h1>
 			<h1>{{ $tryout->age5 }}</h1>
-			<h1><span class="title">Date:</span> {{ date('D F d, Y', strtotime($tryout->date)) }}</h1>
-			<h1><span class="title">Time:</span> {{ date('g:i A', strtotime($tryout->time)) }}</h1>
-			<h1><span class="title">Location:</span> {{ ucwords($tryout->location) }}</h1>
-			<h1><span class="title">City:</span> {{ ucwords($tryout->city) }}, {{ ucwords(stateName($tryout->state)) }}</h1>
-			<h1><span class="title">Contact Name:</span> {{ ucwords($tryout->contact_name) }}</h1>
-			<h1><span class="title">Contact Email:</span> {{ $tryout->contact_email }}</h1>
+			<h1><span class="tryout-title">Date:</span> {{ date('D F d, Y', strtotime($tryout->date)) }}</h1>
+			<h1><span class="tryout-title">Time:</span> {{ date('g:i A', strtotime($tryout->time)) }}</h1>
+			<h1><span class="tryout-title">Location:</span> {{ ucwords($tryout->location) }}</h1>
+			<h1><span class="tryout-title">City:</span> {{ ucwords($tryout->city) }}, {{ ucwords(stateName($tryout->state)) }}</h1>
+			<h1><span class="tryout-title">Contact Name:</span> {{ ucwords($tryout->contact_name) }}</h1>
+			<h1><span class="tryout-title">Contact Email:</span> {{ $tryout->contact_email }}</h1>
 			
 			@if ($tryout->description)
-				<h1><span class="title">Description:</span> {{ $tryout->description }}</h1>
+				<h1><span class="tryout-title">Description:</span> {{ $tryout->description }}</h1>
 			@else
 				<h1>No Additional Information Has Been Provided</h1>
 			@endif

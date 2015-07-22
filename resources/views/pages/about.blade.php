551291aa095110ae8723f8176d939e71527fd53c
Robert Tabor <bobtabor@q.com>
2015-07-22 02:52:46 -0700

    .gitignore is now working

--- resources/views/pages/about.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('app')
-
-@section('content')
-<div class="container">
-	<h1> This is the About page!</h1>
-</div>
-@endsection
--- resources/views/pages/contact.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('app')
-
-@section('content')
-	<div class="container">
-		<h1> This is the Contact page</h1>
-	</div>	
-@endsection

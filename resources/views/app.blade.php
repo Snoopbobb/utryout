767d18b5b90e8543ded1dd0f6fb57e3cef5adc52
Robert Tabor <bobtabor@q.com>
2015-07-28 21:39:40 -0700

    added favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,6 +11,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	<meta name="publishable-key" content="pk_test_Mf4toi2NFZ1E5iGEgPyGrw5c">
 	<title>Utryout | Youth Sports Tryouts</title>
+	<link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
 	<!-- Fonts -->
 	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->

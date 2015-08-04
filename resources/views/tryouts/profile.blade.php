da2bdfeae242477e5428ae8eb49c653805a86916
Robert Tabor <bobtabor@q.com>
2015-08-04 12:28:25 -0700

    url fix

--- resources/views/tryouts/profile.blade.php
+++ resources/views/tryouts/profile.blade.php
@@ -9,7 +9,7 @@
 			@foreach ($tryouts as $tryout)
 				<div class="tryout">
 					<span class="sport"><a href="/tryouts/{{ $tryout->sport }}">{{ $tryout->sport }}</a></span> <h3>{{ $tryout->organization }} | Ages: {{ $tryout->age }}U | {{ date('D F d, Y', strtotime($tryout->date)) }}, {{ date('g:i A', strtotime($tryout->time)) }} | {{ $tryout->city }}, {{ $tryout->state }}</h3>
-					<a href="{{ url('/tryouts/') . '/' . $tryout->sport . '/' . strtolower($tryout->state) . '/' . seoUrl(strtolower($tryout->city)) . '/' .$tryout->id }}">Additional Information</a>
+					<a href="{{ url('/tryouts/') . '/' . $tryout->sport . '/' . strtolower($tryout->state) . '/' . seoUrl(strtolower($tryout->city)) . '/' . $tryout->id . '/' . seoUrl(strtolower($tryout->organization)) }}">Additional Information</a>
 				</div>
 			@endforeach
 				<a href="tryouts/create" class="btn">Post Another Tryout</a>

0aac7ed3f8f42f6dd35c11854688a418d2a38c2c
Robert Tabor <bobtabor@q.com>
2015-08-04 17:41:50 -0700

    sitemap

--- resources/views/pages/sitemap.blade.php
+++ resources/views/pages/sitemap.blade.php
@@ -3,6 +3,7 @@
         xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
         xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
     @foreach( Cache::get('sitemap') as $url => $params )
+    {!! str_replace('nbsp','',$url); !!}
     <url>
         <loc>{{$url}}</loc> 
         <lastmod>{{$params['lastmod']}}</lastmod>

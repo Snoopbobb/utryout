131950163b51259731258340275a84436b6d3084
Robert Tabor <bobtabor@q.com>
2015-08-04 17:02:49 -0700

    sitemap

--- resources/views/pages/sitemap.blade.php
+++ resources/views/pages/sitemap.blade.php
@@ -3,9 +3,9 @@
         xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
         xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
     @foreach( Cache::get('sitemap') as $url => $params )
-    {!! $url = str_replace('&nbsp;','',$url); !!}
+    $url = preg_replace("/\[(.*?)\]\s*(.*?)\s*\[\/(.*?)\]/", "[$1]$2[/$3]", html_entity_decode($url));
     <url>
-        <!-- <loc>{{$url}}</loc>  -->
+        <loc>{{$url}}</loc> 
         <lastmod>{{$params['lastmod']}}</lastmod>
         <changefreq>{{$params['changefreq']}}</changefreq>
         <priority>{{$params['priority']}}</priority>

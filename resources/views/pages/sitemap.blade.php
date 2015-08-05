aec5234289134a90b5ab1f1759023693e34b08b6
Robert Tabor <bobtabor@q.com>
2015-08-04 18:49:26 -0700

    sitemap

--- resources/views/pages/sitemap.blade.php
+++ resources/views/pages/sitemap.blade.php
@@ -4,7 +4,7 @@
         xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
     @foreach( Cache::get('sitemap') as $url => $params )
         <url>
-            <loc>{{ urlencode($url) }}</loc> 
+            <loc>{{ urldecode($url) }}</loc> 
             <lastmod>{{$params['lastmod']}}</lastmod>
             <changefreq>{{$params['changefreq']}}</changefreq>
             <priority>{{$params['priority']}}</priority>

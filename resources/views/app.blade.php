0dae5f4a789f3934fbda12f52d0a08479b261105
Robert Tabor <bobtabor@q.com>
2015-08-09 20:34:11 -0700

    polish

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -91,7 +91,7 @@
 
 	<footer>
 		{{-- <h6>Photo courtesy of <a href="#">Mette McConnell</a></h6> --}}
-		<h6>A <a href="http://30andb.com">Firestackd</a> Application &copy;{{ date('Y') }}</h6>
+		<h6>A <a href="http://30andb.com" style="color: white;">Firestackd</a> Application &copy;{{ date('Y') }}</h6>
 	</footer>
 
 	

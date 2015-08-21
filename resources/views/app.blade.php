069fd1f474de688e183446fef886a9176f685290
Robert Tabor <bobtabor@q.com>
2015-08-21 12:16:16 -0700

    added google tag manager

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -41,6 +41,16 @@
 	<link href="{{ asset('adm/css/main.css') }}" rel="stylesheet">
 </head>
 <body>
+	<!-- Google Tag Manager -->
+		<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-5TSH3S"
+		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-5TSH3S');</script>
+	<!-- End Google Tag Manager -->
+	
 	<nav class="navbar navbar-default navbar-fixed-top">
 		<div class="container-fluid">
 			<div class="navbar-header">

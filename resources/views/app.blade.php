76a5cd50b4246ce45f2b44aad9f1ba12f9ab4a9a
Snoopbobb <bobtabor@q.com>
2016-07-10 00:55:48 -0700

    fixed rsvp css

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,7 +8,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width">
 	<meta name="publishable-key" content="pk_live_rj9Rhg6TBar9ZMp22jcc78n6">
 
 	@if ( empty($title) )
